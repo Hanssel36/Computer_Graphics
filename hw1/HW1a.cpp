674d47792b177315decaf8f43b11e741747b640c
Chris Puntier <cpuntier@gmail.com>
2020-02-20 12:27:21 -0500

    HW1a

--- hw1/HW1a.cpp
+++ hw1/HW1a.cpp
@@ -8,7 +8,8 @@
 // ===============================================================
 
 #include "HW1a.h"
-#include <iostream> // Tempory used to output.
+#include <iostream>
+
 
 // init array of 16 vertices for letter 'P'
 float Vertices[] = {
@@ -63,13 +64,13 @@ HW1a::HW1a(const QGLFormat &glf, QWidget *parent)
 void
 HW1a::initializeGL()
 {
+	
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glColor3f(1.0, 1.0, 1.0);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 	// PUT YOUR CODE HERE
-	// initialize GL function resolution for current context
-	initializeGLFunctions();
-
-	// init state variables
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// set background color
-	glColor3f(1.0F, 1.0f, 1.0f);		// set foreground color
 }
 
 
@@ -84,6 +85,9 @@ void
 HW1a::resizeGL(int w, int h)
 {
 	// PUT YOUR CODE HERE
+	m_winW = w;
+	m_winH = h;
+
 	// compute aspect ratio
 	float ar = (float)w / h;
 	// set xmax, ymax
@@ -96,12 +100,13 @@ HW1a::resizeGL(int w, int h)
 		xmax = 1.;
 		ymax = 1 / ar;
 	}
-	// set viewport to occupy full canvas
+
+	// set viewport
 	glViewport(0, 0, w, h);
 	// init viewing coordinates for orthographic projection
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-xmax, xmax, -ymax, ymax, -1.0, 1.0);
-	
 }
 
 
@@ -114,17 +119,108 @@ HW1a::resizeGL(int w, int h)
 void
 HW1a::paintGL()
 {
-	// PUT YOUR CODE HERE
+	//std::cout << "Hey";
+	//std::cout << Vertices[1];
+
+	///Bottom left draws with points
+	glViewport(0, 0, m_winW/3, m_winH/3);
 	glClear(GL_COLOR_BUFFER_BIT);
-	for (size_t j = 0; j < 9; j++) {
-		glBegin(DrawModes[j]);
-		for (size_t i = 0; i < 31; i++) {
-			glVertex2f(Vertices[i], Vertices[i + 1]);
-			i++;
-		}
+	glBegin(GL_POINTS);
+	for (size_t i = 0; i < 31; i++) {
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
 	}
 	glEnd();
+
+
+	///Bottom middle draws with lines
+	glViewport(m_winW/3, 0, m_winW / 3, m_winH / 3);
+	glBegin(GL_LINES);
+	for (size_t i = 0; i < 31; i++) {
+
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
+	}
+	glEnd();
+
+
+	///Bottom right draws with line strip
+	glViewport(2 * m_winW / 3, 0, m_winW / 3, m_winH / 3);
+	glBegin(GL_LINE_STRIP);
+	for (size_t i = 0; i < 31; i++) {
+
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
+	}
+	glEnd();
+
+
+
+	///middle left draws with line loop
+	glViewport(0, m_winH / 3, m_winW / 3, m_winH / 3);
+	glBegin(GL_LINE_LOOP);
+	for (size_t i = 0; i < 31; i++) {
+
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
+	}
+	glEnd();
+
+	///middle draws with triangles
+	glViewport(m_winW / 3, m_winH / 3, m_winW / 3, m_winH / 3);
+	glBegin(GL_TRIANGLES);
+	for (size_t i = 0; i < 31; i++) {
+
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
+	}
+	glEnd();
+
+	///middle right draws with triangle_strip
+	glViewport(2 * m_winW / 3, m_winH / 3, m_winW / 3, m_winH / 3);
+	glBegin(GL_TRIANGLE_STRIP);
+	for (size_t i = 0; i < 31; i++) {
+
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
+	}
+	glEnd();
+
+	///top left draws with triangle_fan
+	glViewport(0, 2 * m_winH / 3, m_winW / 3, m_winH / 3);
+	glBegin(GL_TRIANGLE_FAN);
+	for (size_t i = 0; i < 31; i++) {
+
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
+	}
+	glEnd();
+
+	///top middle draws with quads
+	glViewport(m_winW / 3, 2 * m_winH / 3, m_winW / 3, m_winH / 3);
+	glBegin(GL_QUADS);
+	for (size_t i = 0; i < 31; i++) {
+
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
+	}
+	glEnd();
+
+	///top right draws with polygon
+	glViewport(2 * m_winW / 3, 2 * m_winH / 3, m_winW/3, m_winH / 3);
+	glBegin(GL_POLYGON);
+	for (size_t i = 0; i < 31; i++) {
+
+		glVertex2f(Vertices[i], Vertices[i + 1]);
+		i++;
+	}
+	glEnd();
+
+
+
 	glFlush();
+	
+	// PUT YOUR CODE HERE
 }
 
 
